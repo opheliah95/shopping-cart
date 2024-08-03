d262aa65ff82b8892746f10f5277e3e26d5dc80c
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-03 14:17:54 +0100

    initial component setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Cart from "./components/Cart";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      Shopping cart
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

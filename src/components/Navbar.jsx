a0cb18476550384431fa81b7f830f0355a1fc6fb
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-05 11:43:55 +0100

    shopping cart initial styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import Bag from "./Icons/Bag"
-import {Link} from "react-router-dom"
+import Bag from "./Icons/Bag";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
       <nav className="nav-bar">
         <Link to="/">
-            <h2>Onlineshop</h2>
+          <h2>Onlineshop</h2>
         </Link>
-        <div className="nav-bag">
-         <Bag/>
-        </div>
-        <span className="bag-quantity">
+        <Link to="/cart">
+          <div className="nav-bag">
+            <Bag />
+            <span className="bag-quantity">
             <span>3</span>
-        </span>
+          </span>
+          </div>
+        </Link>
       </nav>
     </>
   );

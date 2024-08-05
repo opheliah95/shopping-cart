bba38a58e66c09f62d8cf1d4445720917cb59608
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-05 01:53:50 +0100

    initial nav bar setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
+import Bag from "./Icons/Bag"
+import {Link} from "react-router-dom"
 const Navbar = () => {
-    return ( <h2>Navbar</h2> );
-}
- 
+  return (
+    <>
+      <nav className="nav-bar">
+        <Link to="/">
+            <h2>Onlineshop</h2>
+        </Link>
+        <div className="nav-bag">
+         <Bag/>
+        </div>
+        <span className="bag-quantity">
+            <span>3</span>
+        </span>
+      </nav>
+    </>
+  );
+};
+
 export default Navbar;

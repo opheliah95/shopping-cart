97fb413b9b2b712b85e97d293b32f3230263d55a
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-08 12:33:46 +0100

    initial setup to render products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 const Home = () => {
   const { items, status } = useSelector((state) => state.products);
   const { data, error, isLoading } = useGetAllProductsQuery();
+  console.log("data is: ", data);
   return (
     <div className="home-container">
       {isLoading ? (
@@ -13,17 +14,17 @@ const Home = () => {
         <>
           <h2>New Arrivals</h2>
           <div className="products">
-            {data?.map(product => {
+            {data?.map((product) => (
               <div key={product.id} className="product">
-                  <h3>{product.name}</h3>
-                  <img src={product.image} alt={product.name} />
-                  <div className="details">
-                    <span>{product.desc}</span>
-                    <span className="price">£{product.price}</span>
-                  </div>
-                  <button>Add to Cart</button>
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">£{product.price}</span>
+                </div>
+                <button>Add to Cart</button>
               </div>
-            })}
+            ))}
           </div>
         </>
       )}

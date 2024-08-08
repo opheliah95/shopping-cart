d56281988418e4137b1484a39f22cf24a225b068
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-08 12:26:48 +0100

    product component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
 import { useGetAllProductsQuery } from "../features/productsAPI";
+import { useSelector } from "react-redux";
 const Home = () => {
-  const {data, error, isLoading} =useGetAllProductsQuery();
+  const { items, status } = useSelector((state) => state.products);
+  const { data, error, isLoading } = useGetAllProductsQuery();
   return (
-    <>
-      <h2>Home</h2>
-    </>
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occurred {error.data} </p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map(product => {
+              <div key={product.id} className="product">
+                  <h3>{product.name}</h3>
+                  <img src={product.image} alt={product.name} />
+                  <div className="details">
+                    <span>{product.desc}</span>
+                    <span className="price">£{product.price}</span>
+                  </div>
+                  <button>Add to Cart</button>
+              </div>
+            })}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 

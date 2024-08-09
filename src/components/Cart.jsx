e0a5e7fcc775680327a0642b9b2de468971a18d3
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-09 14:22:05 +0100

    wip checkout cart styling

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,58 @@
+import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
+import Arrow from "./Icons/Arrow";
+
 const Cart = () => {
-    return ( <h2>
-        Cart
-    </h2> );
-}
- 
+ const cart = useSelector((state) => state.cart)
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      {cart.cartItems.length === 0 ? (
+            <div className="cart-empty">
+                <p>Your cart is currently Empty</p>
+                <div className="start-shopping">
+                    <Link to="/">
+                    <span>Start Shopping</span>
+                    <Arrow />
+                    </Link>
+                </div>
+            </div>
+      ) : (
+        <div className="cart-display">
+            <div className="titles">
+                <h3 className="product-title">Product</h3>
+                <h3 className="price">Price</h3>
+                <h3 className="quantity">Quantity</h3>
+                <h3 className="total">Total</h3>
+            </div>
+            <div className="cart-items">
+                {
+                    cart.cartItems?.map(
+                        (item) => (
+                            <div className="cart-item" key={item.id}>
+                                <div className="cart-product">
+                                    <div className="cart-image">
+                                        <img src={item.image} alt={item.name} />
+                                    </div>
+                                    <div className="cart-description">
+                                        <h3>{item.name}</h3>
+                                        <p>{item.desc}</p>
+                                        <button>Remove</button>
+                                    </div>
+                                </div>
+                                <div className="cart-product-price">
+                                    £ {item.price}
+                                </div>
+                                <div className="cart-product-quantity"></div>
+                            </div>
+                        )
+                    )
+                }
+            </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Cart;
